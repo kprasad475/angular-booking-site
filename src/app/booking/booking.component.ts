40a3019d617e34dfd54ee800e99f29956f20c637
Binshabs <binshabs567@gmail.com>
2024-08-13 10:45:13 +0530

    images-transitions-added

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { trigger, style, transition, animate } from '@angular/animations';
+
 
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
-  styleUrl: './booking.component.css'
+  styleUrl: './booking.component.css',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('600ms ease-in', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class BookingComponent {
 selectedBus:any;
--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { trigger, style, transition, animate } from '@angular/animations';
+
 
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrl: './payment.component.css'
+  styleUrl: './payment.component.css',
+  animations: [
+    trigger('slideIn', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('500ms ease-in-out', style({ transform: 'translateX(0)' }))
+      ])
+    ])
+  ]
 })
 export class PaymentComponent {
   selectedBus: any;
